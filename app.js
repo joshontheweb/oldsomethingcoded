29d8661e4679fe4b98b773792f04a5352fee0662
Josh Nielsen <Josh@joshontheweb.com>
2011-01-27 13:00:47 -0800

    tidied up

--- app.js
+++ app.js
@@ -38,80 +38,8 @@ socket.on('connection', function(client){
 		client.broadcast(json(message));
 		client.send(json(message));
 	});
-//		try {
-//			request = JSON.parse(message);
-//		} catch (SyntaxError) {
-//			log('Invalid JSON:');
-//			log(message);
-//			return false;
-//		}
-//
-//		if (request.action != 'close' && request.action != 'move') {
-//			log('Invalid request:');
-//			log(message);
-//			return false;
-//		}
-//
-//		request.id = client.id
-//		client.broadcast(JSON.stringify(request));
-//	});
 
 	client.on('disconnect', function(){
 		log('\n\n Disconnected \n\n');
 	});
 });
-
-
-//var express = require('express'),
-//	app = express.createServer();
-//
-//app.get('/', function(req, res){
-//	res.send('Hello World\n');
-//});
-//
-//app.listen(27259);
-//console.log('Express server started on port %s', app.address().port);
-//
-//var io = require('socket.io');
-//var sock = io.listen(app);
-//sock.on('connection', function(conn){
-//	sock.on('message', function(){
-//		try {
-//			request = JSON.parse(message);
-//		} catch (SyntaxError) {
-//			log('Invalid JSON:');
-//			log('message');
-//			return false;
-//		}
-//
-//		if (request.action != 'close' && request.action != 'move') {
-//			log('Invalid request:');
-//			log(message);
-//			return false;
-//		}
-//
-//		request.id = conn.id
-//		conn.broadcast(JSON.stringify(request));
-//	});
-//});
-
-//var http = require('http'),
-//	sys = require('sys'),
-//	io = require('socket.io'),
-//server = http.createServer(
-//	function(req, res){
-//		sys.puts('Request!');
-//		res.writeHeader(200, {'Content-Type': 'text/plain'});
-//		res.writeBody('hello world\n');
-//		res.finish();
-//	}
-//);
-//
-//var socket = io.listen(server);
-//socket.on('connection', function(client){
-//	client.on('message', function(){ var pass = 'pass'});
-//	client.on('disconnect', function(){ var pass = 'pass' });
-//});
-//
-//
-//sys.puts('Server running on port 27259')
